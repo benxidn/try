dcfddac81359ab52402391f6688a15568633b179
Benx Official <107174900+benxidn@users.noreply.github.com>
2024-02-09 23:14:30 +0700

    Add files via upload

---
+++ my-link.js
@@ -0,0 +1,26 @@
+function Home() {
+   window.open("../../index.html","_self");
+   }
+function Channel() {
+   window.open("https://benxidn.github.io/link/subscribe.html","_self");
+   }
+function Unduh() {
+   window.open("https://youtu.be/zQ4KXfQKTho?si=kiW5zMFRySbivJBR","_self");
+   }
+function Drone() {
+   window.open("https://benxidn.github.io/link/subscribe.html","_self");
+   }
+function Fakecez() {
+   window.open("https://benxidn.github.io/link/subscribe.html","_self");
+   }
+function Dexter() {
+   window.open("https://benxidn.github.io/link/subscribe.html","_self");
+   }
+let stringText = '000';
+function copyBtn() {
+   navigator.clipboard.writeText(stringText);
+   }
+let stringText2  = 'SUBSCRIBE';
+function copyBtn2() {
+   navigator.clipboard.writeText(stringText2);
+   }
